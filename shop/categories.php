d9d98806eda37809bcfcdea72e327e030d1bd264
Valerii Petryniak <vpetryniak@e1r12p3.unit.ua>
2020-10-08 04:31:44 +0300

    fr

--- shop/categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysqli = require_once "database.php";
-$query_categories = 'select categories.id, categories.name, count(*) from categories join girls on categories.id = girls.category_id group by categories.id';
-
-$resp = $mysqli->query($query_categories)->fetch_all(MYSQLI_ASSOC);
-
-echo json_encode(['categories' => $resp]);
